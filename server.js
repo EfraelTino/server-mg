246e99367dfeda3b0a9fbec2e7e7eb34bcdc83eb
Miguel Ayala <miguelmrc2022@gmail.com>
2022-12-03 12:32:55 -0500

    restart

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
 import userRouter from './routes/userRoutes.js';
 import orderRouter from './routes/orderRoutes.js';
+import cors from 'cors'
 
 dotenv.config();
 
@@ -19,6 +19,8 @@ mongoose
 
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -38,6 +40,7 @@ app.use((err, req, res, next) =>{
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () =>{
-    console.log(`server at http://localhost:${port}`)
-})
+
+app.listen(port, () => {
+    console.log(`serve at http://localhost:${port}`);
+  });
